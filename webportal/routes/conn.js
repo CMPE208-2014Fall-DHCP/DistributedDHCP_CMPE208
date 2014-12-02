82c4f363fa85b6b1adcfecb8f00d50b17f17ef11
Wei Zhong <wweyeww@gmail.com>
2014-12-01 22:00:55 -0800

    db path

--- webportal/routes/conn.js
+++ webportal/routes/conn.js
@@ -2,8 +2,8 @@
  * Created by leituo56 on 11/23/14.
  */
 var sqlite3 = require('sqlite3').verbose();
-var db_cfg = new sqlite3.Database('db/cfg.db');
-var db_lease = new sqlite3.Database('db/lease.db');
+var db_cfg = new sqlite3.Database('/mnt/glusterfs/cfg.db');
+var db_lease = new sqlite3.Database('/mnt/glusterfs/lease.db');
 
 exports.db_cfg = db_cfg;
 exports.db_lease = db_lease;
